1b0e993158b2fe5c6665ef6084755d15b1288b76
Kevin Wu <kevin.wu@live.com.my>
2018-05-10 11:01:36 +0800

    setting up db and routesessions

--- server/configMongoose.js
+++ server/configMongoose.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'; 
 
 const conf = { 
-  hostname: process.env.MONGO_HOSTNAME || 'localhost', 
-  port: process.env.MONGO_PORT || 27017, 
-  env: process.env.MONGO_ENV || 'local', 
+    hostname: process.env.MONGO_HOSTNAME || 'localhost', 
+    port: process.env.MONGO_PORT || 27017, 
+    env: process.env.MONGO_ENV || 'local', 
 }; 
 
 mongoose.connect(`mongodb://${conf.hostname}:${conf.port}/${conf.env}`,
@@ -11,13 +11,26 @@ mongoose.connect(`mongodb://${conf.hostname}:${conf.port}/${conf.env}`,
     useMongoClient: true
 }); 
 
-const articleSchema = { 
-articleTitle:String, 
-articleContent:String 
-}; 
+const userSchema = { 
+    'username' : String, 
+    'password' : String, 
+    'firstName' : String, 
+    'lastName' : String, 
+    'email' : String, 
+    'role' : String, 
+    'verified' : Boolean, 
+    'imageUrl' : String 
+};
 
+const articleSchema = { 
+    'articleTitle' : String, 
+    'articleContent' : String 
+};
+
+const User = mongoose.model('User', userSchema, 'pubUsers');
 const Article = mongoose.model('Article', articleSchema, 'articles'); 
 
 export default { 
-  Article 
+    User,
+    Article 
 };
--- server/routesSession.js
+++ server/routesSession.js
@@ -1,16 +1,63 @@
-export default [ 
-    {  
-      route: ['login'] , 
-      call: (callPath, args) => 
-        { 
-        const { username, password } = args[0]; 
-  
-        const userStatementQuery = { 
-            $and: [ 
-                { 'username': username }, 
-                { 'password': password } 
-            ] 
-          } 
-        } 
-    } 
-  ];
+import jwt from 'jsonwebtoken'; 
+import crypto from 'crypto'; 
+import jwtSecret from './configSecret';
+import configMongoose from './configMongoose'; 
+
+const User = configMongoose.User;
+
+export default [{  
+    route: ['login'], 
+    call: (callPath, args) => 
+    { 
+        const { username, password } = args[0];
+        const saltedPassword = password + 'pubApp';
+        const saltedPassHash = crypto
+            .createHash('sha256')
+            .update(saltedPassword)
+            .digest('hex');
+        const userStatementQuery = {
+            $and: [
+                { 'username': username },
+                { 'password': saltedPassHash }
+            ]
+        };
+
+        return User.find(userStatementQuery, function(err, user) {
+            if (err) throw err;
+        }).then((result) => {
+            if(result.length) {
+                // Sucessful login
+                const role = result[0].role;
+                const userDetailsToHash = username+role;
+                const token = jwt.sign(userDetailsToHash, jwtSecret.secret);
+                return [{ 
+                    path: ['login', 'token'], 
+                    value: token 
+                }, 
+                { 
+                    path: ['login', 'username'], 
+                    value: username 
+                }, 
+                { 
+                    path: ['login', 'role'], 
+                    value: role 
+                }, 
+                { 
+                    path: ['login', 'error'], 
+                    value: false 
+                }];
+            } else {
+                // failed login
+                return [{ 
+                    path: ['login', 'token'],  
+                    value: 'INVALID' 
+                }, 
+                {
+                    path: ['login', 'error'],  
+                    value: 'NO USER FOUND, incorrect login information' 
+                }]; 
+            }
+            return result; 
+        });
+    }
+}];
